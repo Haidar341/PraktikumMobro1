e2fae2c04b2cf1cac376e61fbbf3a9b416241296
Haidar341 <haizak3@gmail.com>
2024-02-29 19:51:27 +0700

    Praktikum 2

--- app/src/main/java/org/d3if0003/mobro2/MainActivity.kt
+++ app/src/main/java/org/d3if0003/mobro2/MainActivity.kt
@@ -4,8 +4,13 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -14,9 +19,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.d3if0003.mobro2.ui.theme.Mobro2Theme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +40,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Counter()
                 }
             }
         }
@@ -40,26 +51,54 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
  fun MainScreen(content : @Composable (Modifier) -> Unit) {
-     Scaffold(
-         topBar = {
-             TopAppBar(
-                 title = {
-                     Text(text = stringResource(id = R.string.app_name))
-                 },
-                 colors = TopAppBarDefaults.mediumTopAppBarColors(
-                     containerColor = MaterialTheme.colorScheme.primaryContainer,
-                     titleContentColor = MaterialTheme.colorScheme.primary,
-                 )
-             )
-         }
-     ){ padding ->
-         content(Modifier.padding(padding))
-     }
- }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) { padding ->
+        content(Modifier.padding(padding))
+    }
+}
+@Composable
+fun Counter() {
+    var number by remember { mutableIntStateOf(0) }
+    MainScreen { modifier ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = number.toString(),
+                style =  MaterialTheme.typography.displayLarge
+            )
+            Button(
+                onClick = { number ++ },
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(top = 16.dp),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                Text(text = stringResource(R.string.count))
+            }
+        }
+    }
+}
+
 
 @Composable
 fun Greeting(name: String) {
-   MainScreen {Modifier ->
+   MainScreen { modifier ->
        Text(
            text = "Hello $name!",
            modifier = Modifier
@@ -74,6 +113,6 @@ fun Greeting(name: String) {
 @Composable
 fun ScreenPreview() {
     Mobro2Theme {
-        Greeting("Android")
+        Counter()
     }
 }
