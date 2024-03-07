dfa9ccbf960f8476f0b4264fe088b0b7f4806603
Haidar341 <haizak3@gmail.com>
2024-03-07 19:58:00 +0700

    3.4.Debugging Error & Perbaikannya

--- app/src/main/java/org/d3if0003/mobro2/MainActivity.kt
+++ app/src/main/java/org/d3if0003/mobro2/MainActivity.kt
@@ -49,7 +49,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GaleriHewan(data[index]){
-                        index++
+                        index = if (index == data.size-1) 0 else index+1
                     }
 
                 }
